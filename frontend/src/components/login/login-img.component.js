161a8286474741e423d72949277442a60fde95bc
altosoneca <luisguilherme.siqueira32@gmail.com>
2021-03-07 19:22:07 -0300

    finalizando tela de login e iniciando tela de registrar e esquecer senha

--- frontend/src/components/login/login-img.component.js
+++ frontend/src/components/login/login-img.component.js
@@ -1,14 +1,16 @@
 export default function Quadrados() {
 
     function Quadrado(){
+        var d = new Date();
+        var n = d.getDay();
         var cores = ["#12100E", "#30321C" ,"#4A4B2F" ,"#6B654B" ,"#D4DF9E"]
         var cor1 = cores[Math.floor(Math.random()*cores.length)];
         var cor2 = cores[Math.floor(Math.random()*cores.length)];
-        cor2 = cor1 == cor2 ? cores[Math.floor(Math.random()*cores.length)] : cor2;
+        cor2 = cor1 === cor2 ? cores[Math.floor(Math.random()*cores.length)] : cor2;
         var directions = ["to bottom left","to bottom right","to top left","to top right"]
         var direction = directions[Math.floor(Math.random()*directions.length)]
         return (
-            <div style={{background: `linear-gradient(${direction},${cor1} 49%,${cor2} 50%)`}} class="square col-md-2"/>
+            <div style={{background: `linear-gradient(${direction},${cor1} 49%,${cor2} 50%)`}} className="square col-md-2"/>
         )
     }
 
@@ -16,7 +18,7 @@ export default function Quadrados() {
         var rows = [];
 
         for (var i = 0; i < 48; i++) {  
-            rows.push(<Quadrado/> );
+            rows.push(<Quadrado key={i}/> );
         }
         return <div className="row">{rows}</div>
 
--- frontend/src/components/login/login.component.js
+++ frontend/src/components/login/login.component.js
@@ -1,50 +1,69 @@
-import React, { useState } from "react";
-import { Form, Button, Card, Container } from "react-bootstrap";
+import React from "react";
+import { Form, Button, Container } from "react-bootstrap";
 import Quadrados from "./login-img.component"
-
+import { FaDog } from 'react-icons/fa';
 
 
 export default function Login() {
+  
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  // const [error, setError] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+
+  var email="";
+  var password="";
 
   function handleSubmit(event) {
-    console.log(email);
-    console.log(password);
+    console.log(email)
+    console.log(password)
     event.preventDefault();
+  }
+  function handleEmail(event) {
+    email=event.target.value;
+    // setEmail(event.target.value)
+  }
+  function handlePassword(event) {
+    password=event.target.value;
+    // setPassword(event.target.value)
   }
 
   return (
     <div className="row">
 
       <Container className="container-login col-md-6">
-        <Card className="card-login">
-          <Card.Body className="card-body-login">
-            <Form className="form-login" onSubmit={handleSubmit}>
+        
+        <Form className="form-login" onSubmit={handleSubmit}>
+        <h5 className="mb-5 font-italic "><FaDog className="mb-2"/>&nbsp;Petshop</h5>
+        <h1 className="header-login font-weight-bold mb-4">Entrar no Portal de Gestão.</h1>
+        
+          <Form.Group>
+            <Form.Label className="font-weight-normal">Email</Form.Label>
+            <Form.Control className="input-login" onChange={e => handleEmail(e)} type="email" placeholder="name@email.com" />
+          </Form.Group>
 
-              <Form.Group>
-                {/* <Form.Label>Email address</Form.Label> */}
-                <Form.Control onChange={e => setEmail(e.target.value)} type="email" placeholder="Email" />
-              </Form.Group>
+          <Form.Group>
+            <Form.Label className="font-weight-normal">Senha</Form.Label>
+            <Form.Control className="input-login" onChange={e => handlePassword(e)} type="password" placeholder="*********" />
+          </Form.Group>
 
-              <Form.Group>
-                {/* <Form.Label>Password</Form.Label> */}
-                <Form.Control onChange={e => setPassword(e.target.value)} type="password" placeholder="Senha" />
-              </Form.Group>
+          <Form.Group>
+            <Form.Check type="checkbox" label="Me mantenha logado" />
+          </Form.Group>
 
-              <Button className="button-login" variant="primary" type="submit">
-                Login
-            </Button>
-            </Form>
-          </Card.Body>
-        </Card>
+
+          <button onSubmit={e => handleSubmit(e)} className="button-login"  type="submit">
+            Login
+            </button>
+            <div className="d-flex justify-content-center ">
+              <h6 className="header-login font-weight-normal mt-2"><a href="#">Crie uma conta</a> <strong>·</strong> <a href="#">Esqueceu sua senha?</a></h6>
+            </div>
+        </Form>
+
+
       </Container>
 
-
       <Container className="container-login-img col-md-6">
-        <Quadrados/>
+        <Quadrados />
       </Container>
 
 
